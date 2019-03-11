8fd82082ba0c39f6020fd84da9c09bcc4109dc86
Chezka Gaddi <Chezka.Gaddi@mines.sdsmt.edu>
2019-03-11 14:21:52 -0600

    Find difference between both images.

--- import_image.py
+++ import_image.py
@@ -7,9 +7,18 @@ import numpy as np
 import random
 
 img_pop = []
-imgArr = np.asarray(Image.open('panda.jpg'))
+imgArr = np.asarray(Image.open('eye.png'))
 WIDTH = len(imgArr)
 HEIGHT = len(imgArr[0])
+
+def fitness(arr, img):
+    total = 0
+
+    for x in range(WIDTH):
+        for y in range(HEIGHT):
+            total = total + abs(arr[x][y] - img[x][y])
+
+    print(total)
 
 def make_circle(x, y, r, op):
     circle = plt.Circle((x, -y), r, color='black', alpha=op)
@@ -19,52 +28,46 @@ def graph_image(circles):
     plt.clf()
     plt.axis((0, WIDTH, 0, -HEIGHT))
     plt.gca().invert_yaxis()
+    #plt.gca().set_aspect('equal', adjustable='box')
+    plt.gca().set_aspect('equal')
     plt.axis('off')
     ax = plt.gca()
 
-    colors = [100]*len(circles)
+    colors = [0]*len(circles)
     c = PatchCollection(circles, alpha=0.1)
     c.set_array(np.array(colors))
     ax.add_collection(c)
     #for c in circles:
     #    ax.add_patch(c)
 
-    #plt.figure(figsize=(5,5))
+    plt.get_current_fig_manager().resize(WIDTH, HEIGHT)
     plt.subplots_adjust(0,0,1,1,0,0)
     plt.ion()
     plt.show()
     plt.pause(.01)
 
-    plt.get_current_fig_manager().resize(500, 500)
     fig = plt.gcf()
     data = np.frombuffer(fig.canvas.tostring_rgb(), dtype=np.uint8)
     print(fig.canvas.get_width_height())
     data = data.reshape(fig.canvas.get_width_height()[::-1] + (3,))
-    img_pop.append(data)
+    print(len(data), len(data[0]))
+    img_pop.append(Image.fromarray(np.uint8(data)))
 
 def init_pop(popSize, individualSize):
     population = []
 
     for j in range(popSize):
         circles = []
+        h = int(HEIGHT*0.15)
 
         for i in range(individualSize):
-            x, y = random.randint(1,WIDTH), random.randint(1, HEIGHT)
-            radius = random.randint(1, HEIGHT*0.15)
+            x, y = random.randint(1, WIDTH), random.randint(1, HEIGHT)
+            radius = random.randint(1, h)
             circles.append(make_circle(x, y, radius, np.random.rand()))
 
         population.append(circles)
 
     return population
-
-def fitness(arr, img):
-    total = 0
-
-    for x in range(480):
-        for y in range(480):
-            total = total + abs(arr[x][y] - img[x][y])
-
-    print(total)
 
 
 individualSize = 100
@@ -80,8 +83,12 @@ for g in range(generations):
     for q in population:
         graph_image(q)
 
-img = Image.fromarray(np.uint8(img_pop[0]))
-img.save('pic.png')
+img = np.asarray(img_pop[0])
+fitness(imgArr, img)
+print(len(img), len(img[0]))
+
+#img = Image.fromarray(np.uint8(img_pop[0]))
+img_pop[0].save('pic.png')
 
 def circleFitness(circleIndividual, img):
     count = 0
